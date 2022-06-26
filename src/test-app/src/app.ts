3771f0d9d3d531ee94ed0a2a2e3d8c2ad0ff7cbe
ofirdagan <ofirdagan@gmail.com>
2022-06-26 19:03:07 +0300

    step3: setup - use test utils

--- src/test-app/src/app.ts
+++
@@ -1,7 +0,0 @@
-// import { createAppHost } from '../../repluggable';
-import { createAppHost } from 'repluggable';
-import {Foo} from './foo';
-import {Lazy} from './lazy';
-
-const host = createAppHost([Foo]);
-setTimeout(() => host.addShells([Lazy]), 1000);
